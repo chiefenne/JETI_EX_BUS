6a1278ff7e46f311b712cc742f20c2beafb70417
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-04-19 09:59:13 +0200

    initial commit

---
+++ src/JetiEx.py
@@ -0,0 +1,15 @@
+'''Python Implementation of the JETI Ex protocol
+
+This protocol describes how data, text and messages are sent to and from the
+Jeti receiver.
+
+This is used from within the Jeti Ex Bus protocol.
+'''
+
+
+class JetiEx:
+
+    def __init__(self):
+        pass
+
+    
