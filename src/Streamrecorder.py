3b84feec9d6a3ddf14edbcaeae74bda3429b310e
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-04-20 22:50:29 +0200

    removed print

---
+++ src/Streamrecorder.py
@@ -0,0 +1,48 @@
+'''Function to reacord a part of the serial stream
+'''
+
+import uos
+import utime
+from binascii import hexlify
+
+def getStream(serial, logger, duration=1000):
+    '''Write a part of the serial stream to a text file on the SD card 
+    for debugging purposes.
+
+    NOTE: Do not use this function during normal operation.
+            When debugging, use thsi call by 
+
+    NOTE: Writing to the SD card sometimes doesn't work.
+            Do a hard reset when this function is active.
+            After the hard reset the file 'EX_Bus_stream.txt' should exist.
+    '''
+
+    start = utime.ticks_ms()
+    time = 0
+    duration = 1000
+    f = open('EX_Bus_stream.txt', 'w')
+
+    while time < duration:
+
+        buf = bytearray(50)  
+        mv = memoryview(buf)
+        idx = 0
+
+        while idx < len(buf):
+            if serial.any() > 0:
+                bytes_read = serial.readinto(mv[idx:])
+                # print('Got {} bytes of data'.format(bytes_read),
+                #    hexlify(buf[idx:idx+bytes_read], b':'))
+                idx += bytes_read
+
+        f.write(hexlify(buf, b':') + '\n')
+
+        time = utime.ticks_diff(utime.ticks_ms(), start)
+
+    f.close()
+
+    # sync file systems (might be needed?)
+    uos.sync()
+    
+    message = 'EX Bus stream recorded for {} seconds.'.format(duration/1000.)
+    logger.log('debug', message)
