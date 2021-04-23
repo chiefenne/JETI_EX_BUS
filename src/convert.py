7ea9d600106ce2ec7dfdd9478f9e43f9185a2b64
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-04-24 01:32:10 +0200

    renamed function

---
+++ src/convert.py
@@ -0,0 +1,12 @@
+'''Class to store methods for all kinds of conversion
+'''
+
+
+class Converter:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def convert(source, target, value):
+        pass
