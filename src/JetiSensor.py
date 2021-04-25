b1ee7262634018b456f7d3242055c81e524036e2
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-04-25 21:13:55 +0200

    refactoring and logic change

--- src/JetiSensor.py
+++ src/JetiSensor.py
@@ -6,28 +6,47 @@ is used to make up a vario.
 '''
 
 from machine import I2C, Pin
+import ujson
 
 
-class JetiSensor(JetiEx):
-    '''This class represents one or more sensors attached via I2C.
+class Sensors:
+    '''This class represents all sensors attached via I2C
     '''
 
-    def __init__(self, id=1, scl=25, sda=26, freq=400000):
+    def __init__(self):
+        # call constructor of JetiEx
         super().__init__()
 
-
-
-        # known sensors and their I2C addresses
-        # this of course MUST be checked
-        self.known_sensors = {'pressure': {'0x76': 'BME280', '0x77': 'MS5611'},
-                              'gps': {'0x42': 'NEO-M8'}}
-
         self.sensors = dict()
+        # load information of known sensors 
+        self.knownSensors(filename='sensors.json')
 
-        # pin25 and pin26 are the default I2C pins (this is board dependend)
+        # setup I2C connection to sensors
+        self.setupI2C(scl=25, sda=26)
+
+        # get all attached sensors
+        self.scanI2C()
+
+    def setupI2C(self, id=1, scl=25, sda=26, freq=400000):
+        '''Setup an I2C connection.
+        Pins 25 and 26 are the default I2C pins (board dependend)
+        '''
         self.i2c = I2C(id, scl=Pin(scl), sda=Pin(sda), freq=freq)
 
-    def scan(self):
+    def knownSensors(self, filename='sensors.json'):
+        '''Load id, address, type, etc. of known sensors from json file
+        
+        Example sensor.json file {ID: {type, address}}:
+        {
+            "BME280": {"type": "pressure", "address": "0x76"},
+            "MS5611": {"type": "pressure", "address": "0x77"},
+            "NEO-M8": {"type": "gps", "address": "0x42"}
+        }
+        '''
+        with open(filename, 'r') as fp:
+	        self.known_sensors = ujson.load(fp)
+
+    def scanI2C(self):
         '''Scan all I2C addresses between 0x08 and 0x77 inclusive and
         return a list of those that respond.
         '''
@@ -37,15 +56,9 @@ class JetiSensor(JetiEx):
 
         # populate available sensors (subset or all of known sensors)
         for address in addresses:
-            for key in self.known_sensors.keys():
-                if address in self.known_sensors[key].keys():
-                    self.sensors[key] = {address: self.known_sensors[key][address]}
+            for sensor_id in self.known_sensors:
+                if address in self.known_sensors[sensor_id]:
+                    sensor_type = self.known_sensors[sensor_id][0]
+                    self.sensors[sensor_id] = [address, sensor_type]
 
         return self.sensors
-
-    
-
-    def Alarm(self):
-        '''Jeti telemtry alarm protocol.
-        Overwriting method from superclass.
-        '''
