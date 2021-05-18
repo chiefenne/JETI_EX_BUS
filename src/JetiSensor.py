9cc2a89499fb582aaf53243eee5e621f9f947e8a
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-05-18 23:53:03 +0200

    Base class for all sensors

---
+++ src/JetiSensor.py
@@ -0,0 +1,13 @@
+'''Base class for all sensors.
+
+Each sensor needs to be derived from this class.
+
+'''
+
+
+class Sensor:
+    '''Base class for all sensors.
+    '''
+
+    def __init__(self):
+        pass
