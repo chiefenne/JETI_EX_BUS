428aea217a5ab8ae6cc60060d1deab0c379038fb
Andreas Ennemoser <andreas.ennemoser@aon.at>
2021-05-19 12:51:07 +0200

    move read to sensor class

--- src/I2C.py
+++ src/I2C.py
@@ -103,34 +103,3 @@ class Devices:
                 ms5611 = MS5611.MS5611(bus=self.i2c)
                 self.available_sensors[sensor]['reader'] = ms5611
 
-    def read(self, sensor):
-        '''Read data from sensor.
-
-        The data are read in different ways, depending on the snesor (driver)
-
-        Args:
-            sensor (str): Sensor id
-        '''
-
-        # BME280 pressure sensor
-        if 'BME280' in sensor:
-            address = self.available_sensors[sensor]['address']
-            reader = self.available_sensors[sensor]['reader']
-            t, p, h = reader.values
-            message = 'Sensor: {}({}), Pressure {}, Temperature {}' \
-                .format(sensor, address, p, t)
-            # self.logger.log('info', message)
-
-            return p, t, h
-
-        # MS5611 pressure sensor
-        if 'MS5611' in sensor:
-            address = self.available_sensors[sensor]['address']
-            reader = self.available_sensors[sensor]['reader']
-            reader.read()
-            message = 'Sensor: {}, Address {},Pressure {}, Temperature {}'. \
-                        format(sensor, address, p, t)
-            self.logger.log('info', message)
-
-            return reader.pressureAdj, reader.tempC
-
