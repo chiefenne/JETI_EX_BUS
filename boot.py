83907b1af68556a5341dc48f0bf2ec22f4baed1b
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-05-08 12:21:04 +0200

    add boot.py to distribution

---
+++ boot.py
@@ -0,0 +1,41 @@
+# boot.py -- run on boot-up
+# this script is executed when the pyboard boots up. It sets
+# up various configuration options for the pyboard.
+# can run arbitrary Python, but best to keep it minimal
+
+# IMPORTANT:
+# When the pyboard boots up, it needs to choose a filesystem to boot from.
+# If there is no SD card, then it uses the internal filesystem /flash as the boot filesystem,
+# otherwise, it uses the SD card /sd.
+# After the boot, the current directory is set to one of the directories above.
+# If needed, you can prevent the use of the SD card by creating an empty file called /flash/SKIPSD.
+# If this file exists when the pyboard boots up then the SD card will be skipped and
+# the pyboard will always boot from the internal filesystem
+# (in this case the SD card won’t be mounted but you can still mount and
+# use it later in your program using os.mount).
+
+import usys as sys
+from os import listdir as ls
+print (' ')
+print ('Imported os and os.listdir as ls')
+print ('You can use now ls() to list directory contents.')
+print (' ')
+
+# check if we are on a Pyboard
+pyboard = False
+if 'pyboard' in sys.platform:
+    import pyb
+    pyboard = True
+    print('We are on a pyboard')
+else:
+    print('We are on a {}'.format(sys.platform))
+
+from ubinascii import hexlify, unhexlify
+
+# main script to run after this one
+# if not specified "main.py" will be executed
+# pyb.main('irgendwas.py')
+
+# run pyboard in different modes
+# pyb.usb_mode('CDC+MSC') # act as a serial and a storage device
+# pyb.usb_mode('CDC+HID') # act as a serial device and a mouse
