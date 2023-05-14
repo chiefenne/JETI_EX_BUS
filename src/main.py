19f6cf88302c673f3795bf38ce5e340e53624e4f
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-05-15 00:01:17 +0200

    reworked for multi-core processing

--- src/main.py
+++ src/main.py
@@ -18,29 +18,71 @@ Date: 04-2021
 # are stripped down in MicroPython to be efficient on microcontrollers
 import usys as sys
 import uos as os
+import _thread
 
 from Jeti.Serial_UART import Serial
 from Jeti.ExBus import ExBus
-from Sensors.Sensor import Sensors
+from Jeti.Ex import Ex
+from Sensors.Sensors import Sensors
 from Utils.Logger import Logger
+from Utils.Streamrecorder import saveStream
+from Utils.round_robin import cycler
 
 
 # setup a logger for the REPL
 logger = Logger(prestring='JETI MAIN')
 
-# print some information about the board
-logger.log('info', 'Board: {}'.format(os.uname().machine))
-logger.log('info', 'MicroPython version: {}'.format(sys.version))
-logger.log('info', 'MicroPython build: {}'.format(sys.implementation.name))
-
 # Serial connection bewtween Jeti receiver and microcontroller
 # baudrate=125000, 8-N-1
 serial = Serial()
-serial.connect()
+
+# write 1 second of the serial stream to a text file for debugging purposes
+DEBUG = False
+if DEBUG:
+    saveStream(serial, logger, filename='EX_Bus_stream.txt', duration=1000)
 
 # collect sensors attached to the microcontroller via I2C
 sensors = Sensors()
 
-# run JETI Ex Bus protocol and handle all data streams
-exbus = ExBus(serial, sensors)
-exbus.run_forever()
+# setup the JETI EX protocol
+ex = Ex()
+
+# setup the JETI EX BUS protocol
+exbus = ExBus(serial, sensors, ex)
+
+# function which is run on core 0
+def core0():
+
+    while True:
+
+        # debug
+        exbus.dummy()
+
+        #exbus.run_forever()
+
+# function which is run on core 1
+def core1():
+
+    cycle_sensors = cycler(sensors.get_sensors())
+    
+    while True:
+
+        # cycle infinitely through all sensors
+        sensor = next(cycle_sensors)
+
+        # collect data from currently selected sensor
+        sensors.read(sensor)
+
+        # debug
+        ex.dummy()
+
+        # compile data into a JETI EX frame
+        # frame = ex.ex_frame(sensors.get_data(), type='data')
+
+# start the second thread on core 1
+logger.log('info', 'Starting second thread on core 1')
+second_thread = _thread.start_new_thread(core1, ())
+
+# run the main loop on core 0
+logger.log('info', 'Starting main loop on core 0')
+core0()
