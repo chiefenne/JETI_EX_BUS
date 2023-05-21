4c6d70a6dc0e9c7951608fe806cce5b83b4318aa
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-05-21 10:39:58 +0200

    initial versions of debugging tools

---
+++ debug/EX_BUS_packet_decoder.py
@@ -0,0 +1,32 @@
+'''Decode a Jeti EX BUS packet'''
+
+import ustruct
+
+
+class DecodeExBusPacket:
+
+    def __init__(self, packet):
+        self.packet = packet
+
+    def decode(self):
+        '''Decode a Jeti EX BUS packet'''
+
+        # unpack the packet
+        data = us
+
+    def print(self):
+        '''Print the decoded packet'''
+
+        print('EX BUS Packet: {}'.format(self.packet))
+
+
+if __name__ == '__main__':
+
+    # test data
+    packet = b
+
+    # decode the packet
+    decode = DecodeExBusPacket(packet)
+
+    # print the decoded packet
+    decode.print()
--- debug/debug_serial_connection.py
+++ debug/debug_serial_connection.py
@@ -11,4 +11,26 @@ from Utils.Logger import Logger
 
 
 # setup a logger for the REPL
-logger = Logger(prestring='JETI MAIN')
+logger = Logger(prestring='JETI SERIAL')
+
+# Serial connection bewtween Jeti receiver and microcontroller
+# defaults: baudrate=125000, 8-N-1
+s = Serial(port=0)
+serial = s.connect()
+
+# read 1 second of the serial stream to a text file for debugging purposes
+DEBUG = True
+if DEBUG:
+    logger.log('info', 'DEBUG mode is ON')
+    logger.log('info', 'Writing EX_Bus_stream.txt')
+    with open('EX_Bus_stream.txt', 'w') as f:
+        for i in range(1000):
+            f.write(str(serial.read(1)))
+    logger.log('info', 'Writing EX_Bus_stream.txt finished')
+else:
+    logger.log('info', 'DEBUG mode is OFF')
+    logger.log('info', 'Writing EX_Bus_stream.txt is disabled')
+
+# close the serial connection
+s.disconnect()
+
