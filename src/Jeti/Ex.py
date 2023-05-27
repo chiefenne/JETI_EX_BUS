010f0427025a8fccc533d048a3bae7655c8e6ba4
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-05-27 18:53:01 +0200

    reduce function docs

--- src/Jeti/Ex.py
+++ src/Jeti/Ex.py
@@ -69,8 +69,7 @@ class Ex:
         pass
 
     def exbus_frame(self, sensor):
-        '''Send telemetry data back to the receiver (master).
-        '''
+        '''Prepare the EX BUS telemetry packet.'''
 
         # setup ex packet
         self.ex_packet = self.ex_frame(sensor)
@@ -105,11 +104,7 @@ class Ex:
         return self.exbus_packet
 
     def ex_frame(self, sensor):
-        '''Compile the EX telemetry packet (Header, data or text, etc.)
-
-        Returns:
-            packet (hex): The complete packet describing the telemetry
-        '''
+        '''Compile the EX telemetry packet (Header, data or text, etc.).'''
         self.current_sensor = sensor
 
         # get sensor data
@@ -145,29 +140,7 @@ class Ex:
         return self.ex_packet
 
     def Header(self, packet_type, length):
-        '''EX packet message header
-
-         Byte |  Length |     Data     |  Description
-        ------|---------|--------------|----------------------------------------
-          1   |    1B   |     0x7E     |  Separator of the message (not needed if EX frame is embedded in EX BUS frame)
-          2   |    1B   |     0xNF     |  Distinct identifier of an EX packet (N is an arbitrary number)
-          3   |    2b   |  Type (0-3)  |  0 - Text, 1 – Data, 2 – Message
-          3   |    6b   | Length (0-31)|  Length of a packet (number of bytes following)
-         4-5  |    2B   |       SN     |  Upper part of a sensors serial number, Manufacturer ID (Little Endian)
-         6-7  |    2B   |       SN     |  Lower part of a sensors serial number, Device ID (Little Endian)
-          8   |    1B   |     0x00     |  Reserved
-     
-        NOTE: Byte 3 of the packet is split into 2 and 6 bits
-        NOTE: The upper part of the serial number should be in the range A400 – A41F
-              The lower part of a serial number should be used in a manner it
-              ensures uniqueness of the whole serial number
-        NOTE: Maximum allowed length of a packet is 29B (together with separators 0x7E; 0xNF)
-     
-        Bytes 3 to 8 make up the message header. Depending on byte 3 [2bits] which
-        is the packet type, there follow three different specifications (data, text, message)
-        for the rest of the bytes of the packet.
-        
-        '''
+        '''EX packet message header.'''
 
         header = bytearray()
 
@@ -203,28 +176,7 @@ class Ex:
         return header
 
     def Data(self):
-        '''EX packet data
-
-        Byte  |  Length |    8 data bits    |  Description
-       -------|---------|-------------------|----------------------------------------
-         9    |    4b   | Identifier (0-15) |  Identifier of telemetry value
-         9    |    4b   |  Data type (0-15) |  Data type of telemetry value
-        10    |    xB   |        Data       |  Data with length according to data type
-        11+x  |    4b   | Identifier (1-15) |  Identifier of a second telemetry value
-        11+x  |    4b   |  Data type (0-15) |  Data type of a second telemetry value
-        12+x  |    yB   |        Data       |  Data with length according to data type
-        13+x+y|    1B   |        CRC8       |  Cyclic redundancy check
-
-        Telemetry example:
-            value = 115.3
-        Convert scaled value to hex:
-            hex(1153) --> '0x 481'
-        Convert sign (0 for +, 1 for -) and decimal point position (01):
-            hex(001) --> '0x1'
-        Combine results:
-            '0x 1481'
-
-        '''
+        '''EX packet data.'''
 
         self.data = bytearray()
 
@@ -277,18 +229,7 @@ class Ex:
         return self.data, len(self.data)
 
     def Text(self):
-        '''EX packet text
-
-        Byte  |  Length |            8 data bits          |  Description
-       -------|---------|---------------------------------|----------------------------------------
-         9    |    1B   |       Identifier (0-255)        |  Identifier of telemetry value
-        10    |    5b   | Length of the description (x)   |  Length of the description in Bytes
-        10    |    3b   | Length of unit's description (y)|  Length of the unit's description in Bytes
-        11    |    xB   |                Label            |  ASCII textual description of a value
-        11+x  |    yB   |                Label            |  ASCII textual description of a unit
-        11+x+y|    1B   |                 CRC8            |  Cyclic redundancy check
-
-        '''
+        '''EX packet text.'''
 
         if self.current_sensor._type == 'pressure':
 
@@ -314,68 +255,18 @@ class Ex:
         return self.text, len(self.text)
 
     def Message(self):
-        '''
-
-         Byte   |  Length |       8 data bits         |  Description
-        --------|---------|---------------------------|----------------------------------------
-          9     |    1B   |    Message type (0-255)   |  Primary identifier of the message type.
-                |         |                           |  Value is used for additional semantic information
-                |         |                           |  and localization.
-         10[7:5]|    3b   |    Message class          |  Class identifier of the message type.
-                |         |                           |  Gives additional semantics
-         10[4:0]|    5b   | Length of the message (x) |  Length of the message in bytes
-         11     |    xB   |          Message          |  UTF-8 message
-         11+x   |    1B   |             CRC8          |  Cyclic redundancy check
-     
-        Message class semantics:
-      
-            Message class  |   Description
-        ----------------|----------------------------------------------------
-                0       | Basic informative message (really unimportant messages)
-                1       | Status message (device ready, motors armed, GPS position fix etc.)
-                2       | Warning (alarm, high vibrations, preflight conditions check, …)
-                3       | Recoverable error (loss of GPS position, erratic sensor data, …)
-                4       | Nonrecoverable error (peripheral failure, unexpected hardware fault, …)
-                5       | Reserved
-                6       | Reserved
-                7       | Reserved
-
-        '''
+        '''EX packet message.'''
 
         pass
 
     def Alarm(self):
-        '''EX packet alarm
-
-        Protocol includes a letter encoded in Morse Code alphabet, which is later acoustically signalized
-
-         Byte  | 8 data bits| Bit 8 |   Description
-        -------|------------|-------|-------------------------------
-          1    |    0x7E    |    0  | Separator of a message
-          2    |    0xNL    |    1  | L = number of bytes following (always 2), N can be any number
-          3    |  0x22/0x23 |    1  | 0x22- without reminder tone (Vario); 0x23 – with reminder tone (standard alarm)
-          4    |   'A'-'Z'  |    1  | ASCII letter to be signalized by Morse alarm
-
-        '''
+        '''EX packet alarm.'''
+        pass
 
     def SimpleText(self, text):
-        '''EX packet simple text (must be 34 bytes long)
+        '''EX packet simple text (must be 34 bytes long).
         32 bytes of text are needed + 2 bytes for the separators.
-
         The simple text is concatenated to every telemetry packet.
-
-        Byte  |  Length |8 data bits|  Bit 8 |   Description
-       -------|---------|------------|-------|-----------------------------
-         1    |    1B   |    0xFE    |    0  | Separator of a message (begin)
-         2    |    1B   | 'T' (0x54) |    1  | ASCII character
-         3    |    1B   | 'E' (0x45) |    1  | ASCII character
-         4    |    1B   | 'X' (0x58) |    1  | ASCII character
-         5    |    1B   | 'T' (0x54) |    1  | ASCII character
-       ...    |   ...   |     ...    |    0  | ...
-       ...    |   ...   |     ...    |    0  | ...
-       ...    |   ...   |     ...    |    0  | ...
-        34    |    1B   |    0xFF    |    0  | Separator of a message (end)
-
         '''
 
         # crop text if too long, fill up if needed, left adjusted
@@ -394,41 +285,3 @@ class Ex:
         self.simple_text += b'FF'
 
         return self.simple_text
-
-    def value_to_EX(self, value=None, nbytes=2, precision=1, endian='little'):
-        '''Convert a value to the EX protocol specification
-
-        Args:
-            value (int, float): Any telemetry value
-            nbytes (int): Number of bytes (uint6_t = 1, uint14_t = 2, etc.)
-            precision ([type]): [description]
-
-        Returns:
-            (hex): Hex string describing the value
-        '''
-
-        # get the sign of the value
-        sign = 0 if value >= 0x0 else 1
-
-        # use value without sign (as this is transferred on MSB)
-        value = abs(value)
-
-        # scale value according to precision
-        scaled_value = int(value * 10**precision) if precision > 0 else int(value)
-
-        # compile hex string from above
-        val = sign << (nbytes*8 - 1) | precision << (nbytes*8 - 3) | scaled_value
-        hex_str = hex(val)[2:]
-
-        # compile even number of hex (add leading "0")
-        if len(hex_str) % 2 == 1:
-            hex_str = '0' + hex_str
-
-        # split hex into list of pairs
-        hex_str = [hex_str[i:i+2] for i in range(0,len(hex_str), 2)]
-	
-	    #  reverse bytes if little endian is required
-        if endian == 'little':
-            hex_str.reverse()
-
-        return [e.encode('utf-8') for e in hex_str]
