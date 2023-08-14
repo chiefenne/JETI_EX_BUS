bec586590c640f771d9f528a6c8b1cd4b2fc45e4
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-08-14 16:58:00 +0200

    measure frequency on pin

--- src/Utils/frequency_counter.py.py
+++ src/Utils/frequency_counter.py.py
@@ -1,25 +1,31 @@
 
-import machine
+from machine import Pin, Timer
 
 
 class FrequencyCounter:
-    def __init__(self, pin_num):
-        self.pin = machine.Pin(pin_num, machine.Pin.IN)
-        self.timer = machine.Timer(-1)
+    def __init__(self, pin_num, readout=100):
+        '''Read frequency from a GPIO pin
+        
+        Args:
+            pin_num (int): Pin number
+            readout (int, optional): Frequency readout interval in ms.
+        
+        '''
+        self.pin = Pin(pin_num, Pin.IN, Pin.PULL_DOWN)
+        self.timer = Timer()
+        self.timer.init(freq=readout,
+                        mode=Timer.PERIODIC,
+                        callback=self.timer_callback)
         self.counter = 0
 
         # Setup interrupt for rising edges
-        self.pin.irq(trigger=machine.Pin.IRQ_RISING, handler=self.irq_handler)
+        self.pin.irq(trigger=Pin.IRQ_RISING, handler=self.irq_handler)
 
     def irq_handler(self, pin):
         self.counter += 1
 
-    def start(self, duration_ms=1000):
-        self.counter = 0
-        self.timer.init(period=duration_ms,
-                        mode=machine.Timer.PERIODIC,
-                        callback=self.timer_callback)
 
     def timer_callback(self, timer):
+        # frequency = counts / (timer period in ms) / 1000
+        self.frequency = self.counter / timer.period() / 1000
         self.counter = 0
-        self.frequency = self.counter / timer.period()
