ccf03add248575d9f1e309f84e18ec5ff33ab1a8
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-08-14 22:47:36 +0200

    cleanup

--- src/boot.py
+++ src/boot.py
@@ -14,19 +14,11 @@ print('JETI BOOT - INFO: Platform:', sys.platform)
 print('JETI BOOT - INFO: Operating system:', sys.version)
 print('JETI BOOT - INFO: Underlying machine:', sys.implementation._machine)
 
-
 # blink led 's' seconds with frequency 'hz'
 def blink(led, s, hz):
     for i in range(s*hz):
         led.toggle()
         time.sleep(1/hz)
-
-# check if we are on a Pyboard
-# initially this was the development platform
-if 'pyboard' in sys.platform:
-    import pyb
-    blue = pyb.LED(4)
-    blue.on()
 
 # switch off leds on TINY 2040 (they are on by default)
 if 'rp2' in sys.platform:
@@ -48,8 +40,3 @@ if 'rp2' in sys.platform:
 
 # main script to run after this one
 # if not specified "main.py" will be executed
-
-# here we import the main script; this runs all the code in main.py
-# this is useful when no code is on the board yet and mpremote is used
-# to upload the code to the board; for small projects only
-# import main
