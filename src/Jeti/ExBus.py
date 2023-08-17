4108f067e52af75c0af5c0779fece20e2343d18e
Andreas Ennemoser <andreas.ennemoser@aon.at>
2023-08-17 11:26:06 +0200

    declutter code

--- src/Jeti/ExBus.py
+++ src/Jeti/ExBus.py
@@ -17,13 +17,9 @@ from ubinascii import hexlify
 import utime
 import micropython
 from micropython import const
-from machine import WDT
 
 from Jeti import CRC16
 from Utils.Logger import Logger
-
-# enable the WDT with (1s is the minimum)
-# wdt = WDT(timeout=5000)
 
 
 class ExBus:
@@ -37,7 +33,7 @@ class ExBus:
         self.frame_count = -1
 
         # number of frames for sending initial device and label information
-        self.label_frames = 200
+        self.label_frames = 100
 
         
         # lock object used to prevent other cores from accessing shared resources
@@ -50,14 +46,17 @@ class ExBus:
 
     @micropython.native
     def run_forever(self):
-        '''This is the main loop and will run forever. This function is called
-        at the end of the function "main.py". It does exactly the same as the
-        Arduino "loop()" function.
+        '''This is the main loop and will run forever.
 
         Within the loop the datastream of the EX bus is checked for:
-          1) Telemetry request (a sensor can send data)
-          2) JetiBox request (modify parameters)
-          3) Channel data (current status of the transmitter)
+          - Channel data (current status of the transmitter controls)
+          - Telemetry request (a sensor can send data)
+          - JetiBox request (modify parameters)
+
+        NOTE: bytearrays needs slicing in order to return a byte and not an integer
+              buffer[0] returns an integer
+              buffer[0:1] returns a byte
+
         '''
 
         # define states of the EX bus protocol
@@ -73,7 +72,6 @@ class ExBus:
 
         # initialize the state
         state = STATE_HEADER_1
-
         while True:
 
             # read one byte from the serial stream
@@ -84,11 +82,7 @@ class ExBus:
 
                 # check for EX bus header 1
                 if c in [b'\x3e',  b'\x3d']:
-
-                    # initialize the buffer
                     buffer = bytearray()
-
-                    # add the first byte to the buffer
                     buffer += bytearray(c)
                     
                     # change state
@@ -97,16 +91,11 @@ class ExBus:
             elif state == STATE_HEADER_2:
                 # check for EX bus header 2
                 if c in [b'\x01', b'\x03']:
-                
                     buffer += bytearray(c)
 
                     # change state
                     state = STATE_LENGTH
                 
-                # else:
-                #     # reset state
-                #     state = STATE_HEADER_1
-
             elif state == STATE_LENGTH:
                 # check for EX bus packet length
                 buffer += bytearray(c)
@@ -116,10 +105,7 @@ class ExBus:
 
                 # check if packet length is valid
                 # 6 bytes header + max. 24*2 bytes data + 2 bytes CRC
-                # FIXME:
-                # FIXME: check if above is correct (check also in STATE_END)
-                # FIXME:
-                if packet_length > 64:
+                if packet_length > 60:
                     # reset state
                     state = STATE_HEADER_1
                     continue
@@ -129,100 +115,63 @@ class ExBus:
 
             elif state == STATE_END:
 
-                if len(buffer) > 64:
+                if len(buffer) > 60:
                     # reset state
                     state = STATE_HEADER_1
                     continue
 
-                # check for rest of EX bus packet
-                # ID, data identifier, data, CRC
+                # update rest of EX bus packet (ID, data identifier, data, CRC)
                 buffer += bytearray(c)
                 
                 # check if packet is complete
                 if len(buffer) == packet_length:
                     
                     # check CRC
-                    if self.checkCRC(buffer):
-                        # packet is complete and CRC is correct
+                    if self.checkCRC(buffer): # packet is complete and CRC is correct
     
-                        # NOTE: accessing the bytearray needs slicing in order
-                        #       to return a byte and not an integer
-                        #       buffer[0] returns an integer
-                        #       buffer[0:1] returns a byte
-                        #       this way no conversion is needed
-
                         # check for channel data
                         if buffer[0:1] == b'\x3e' and \
                            buffer[4:5] == b'\x31':
-                            # get the channel data
                             self.getChannelData(buffer)
 
                         # check for telemetry request
-                        elif buffer[0:1] == b'\x3d' and \
-                             buffer[1:2] == b'\x01' and \
-                             buffer[4:5] == b'\x3a':
-                            
-                            packetID = buffer[3:4]
-
-                            # send telemetry data
-                            self.sendTelemetry(packetID)
+                        elif buffer[:2] == b'\x3d\x01' and buffer[4:5] == b'\x3a':
+                            self.sendTelemetry(buffer[3:4])
 
                         # check for JetiBox request
-                        elif buffer[0:1] == b'\x3d' and \
-                             buffer[1:2] == b'\x01' and \
-                             buffer[4:5] == b'\x3b':
-                            # send JetiBox menu data
+                        elif buffer[:2] == b'\x3d\x01' and buffer[4:5] == b'\x3b':
                             self.sendJetiBoxMenu()
 
                     # reset state
                     state = STATE_HEADER_1
                     continue
 
-            # feed watchdog (comment out for debugging)
-            # wdt.feed()
-
     @micropython.native
-    def getChannelData(self, buffer, verbose=False):
+    def getChannelData(self, buffer):
         self.channel = dict()
         
         num_channels = int.from_bytes(buffer[5:6], 'little') // 2
 
-        if verbose:
-            self.logger.log('info', 'Number of channels: ' + str(num_channels))
-
         for i in range(num_channels):
             self.channel[i] = buffer[6 + i*2 : 7 + i*2] + \
                               buffer[7 + i*2 : 8 + i*2]
-            
-            if verbose:
-                self.logger.log('info',
-                    'Channel: ' + str(i+1) + 
-                    ' Value: ' + str(int.from_bytes(self.channel[i], 'little') / 8000)
-                               + ' ms')
-    
+
     @micropython.native
-    def sendTelemetry(self, packetID, verbose=False):
+    def sendTelemetry(self, packetID):
         '''Send telemetry data back to the receiver (master).
 
         The packet ID is required to answer the request with the same ID.
         '''
 
-        t_start = utime.ticks_us()
-
         # frame counter
         self.frame_count += 1
 
         # acquire lock to access the "ex" object" exclusively
-        # core 1 cannot acquire the lock if core 0 has it
         self.lock.acquire()
-        t_lock = utime.ticks_us()
 
         if self.ex.exbus_device_ready and self.frame_count <= self.label_frames:
-            # send device and label information repeatedly within the first
-            # self.label_frames frames; the transmitter stores the information
-            # and associates later the labels with the telemetry data by their id
-            n_labels = len(self.ex.dev_labels_units)
-            telemetry = self.ex.dev_labels_units[self.frame_count % n_labels]
+            # send device and label information (cycle through labels)
+            telemetry = self.ex.dev_labels_units[self.frame_count % self.ex.n_labels]
 
         elif self.ex.exbus_data_ready and self.frame_count > self.label_frames:
             # send telemetry values
@@ -238,9 +187,6 @@ class ExBus:
             self.lock.release()
 
         # packet ID (answer with same ID as by the request)
-        # slice assignment is required to write a byte to the bytearray
-        # it does an implicit conversion from byte to integer
-        # telemetry[3:4] = packetID
         telemetry_ID = telemetry[:3] + packetID + telemetry[4:]
 
         # calculate the crc for the packet (as the packet is complete now)
@@ -250,16 +196,7 @@ class ExBus:
         telemetry_ID_CRC16 = telemetry_ID + crc16_int.to_bytes(2, 'little')
 
         # write packet to the EX bus stream
-        # bytes_written = self.serial.write(telemetry)
         bytes_written = self.serial.write(telemetry_ID_CRC16)
-
-        t_end = utime.ticks_us()
-        # log lock time
-        diff = utime.ticks_diff(t_lock, t_start)
-        # self.logger.log('info', 'EX BUS lock acquired in {} us'.format(diff))
-        # log the time for sending the packet
-        diff = utime.ticks_diff(t_end, t_start)
-        # self.logger.log('info', 'EX BUS telemetry sent in {} us'.format(diff))
 
         return bytes_written
 
